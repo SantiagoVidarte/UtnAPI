72a6ebc9680bf9000bf501c2244ea49a7cec3038
Santiago Martin Vidarte <smvidarte@ONP.mecon.ar>
2024-08-12 15:23:06 -0300

    final commit

--- routes/movies.js
+++ routes/movies.js
@@ -2,9 +2,9 @@ import { Router } from 'express'
 import {movieController} from '../controller/movies.js';
 import { token } from '../services/jwt.js';
 export const router = Router();
-router.get("/", token.validate,movieController.getAllMovies);
+router.get("/",movieController.getAllMovies);
 router.get("/s",movieController.getByTitle );
 router.get("/:id", (req, res) => res.send(`List a movie by id:${req.params.id}`));
-router.delete("/:id", movieController.deleteOne);
-router.patch("/:id",movieController.updateOne);
-router.post("/", movieController.createOne);
+router.delete("/:id", token.validate, movieController.deleteOne);
+router.patch("/:id",token.validate,movieController.updateOne);
+router.post("/",token.validate, movieController.createOne);
--- services/jwt.js
+++ services/jwt.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config();
 const secret = process.env.JWT_SECRET;
 
 if (!secret) {
   throw new Error('JWT_SECRET is not defined');
 }
+
 export const token = {
   async generate(user) {
     const userForToken = {
